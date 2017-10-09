40e80266411f8c3883be398da900c41cc488c43c
Zachary Smith <zach.smith33@gmail.com>
2017-10-08 20:18:56 -0400

    fix weback config

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,28 @@
-var path = require('path');
+var path = require('path')
+var webpack = require('webpack')
 
 module.exports = {
   entry: './src/index.ts',
   output: {
     path: path.resolve(__dirname, './dist'),
     publicPath: '/dist/',
-    fileName: 'build.js'
+    filename: 'build.js'
   },
-  modules: { 
+  module: {
     rules: [
       {
         test: /\.vue$/,
         loader: 'vue-loader',
-        loaders: {
-          'scss': 'vue-style-loader!css-loader!sass-loader',
-          'sass': 'vue-style-loader!css-loader!sass-loader?indentedSyntax',
+        options: {
+          loaders: {
+            // Since sass-loader (weirdly) has SCSS as its default parse mode, we map
+            // the "scss" and "sass" values for the lang attribute to the right configs here.
+            // other preprocessorsould work out of the box, no loader config like this necessary.
+            'scss': 'vue-style-loader!css-loader!sass-loader',
+            'sass': 'vue-style-loader!css-loader!sass-loader?indentedSyntax',
+          }
+          // other vue-loader options go here
         }
-
       },
       {
         test: /\.tsx?$/,
